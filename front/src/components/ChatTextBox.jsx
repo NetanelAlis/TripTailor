1c63caaccee75bfe45370084fb3aeb8dc5000773
NetanelAlis <netanelalis11@gmail.com>
2025-04-01 19:25:32 +0300

    added ChatTextBox component

---
+++ front/src/components/ChatTextBox.jsx
@@ -0,0 +1,44 @@
+import React, { useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlaneDeparture } from '@fortawesome/free-solid-svg-icons';
+
+function ChatTextBox() {
+  const chatBox = useRef(null);
+  const [userInput, setUserInput] = useState('');
+
+  return (
+    <>
+      <div className="chat-box">
+        <textarea
+          className="chat-input"
+          placeholder="Ask anything"
+          rows="1"
+          ref={chatBox}
+          onInput={(e) => {
+            e.target.style.height = 'auto';
+            e.target.style.height = `${e.target.scrollHeight}px`;
+            setUserInput(e.target.value);
+          }}
+          value={userInput}
+        />
+      </div>
+      <div className="chat-footer">
+        <button
+          className={
+            userInput !== ''
+              ? 'chat-submit send-button'
+              : 'chat-submit-disabled send-button'
+          }
+          onClick={() => {
+            chatBox.current.style.height = 'auto';
+            setUserInput('');
+          }}
+        >
+          <FontAwesomeIcon icon={faPlaneDeparture} />
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default ChatTextBox;
