d25355b8454849616dbae335d1add15084c74625
NetanelAlis <netanelalis11@gmail.com>
2025-04-09 19:18:02 +0300

    Implement RootLayout component and update routing
     modify styles for chat and new chat page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,16 +1,23 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import NewChat from './pages/new-chat/NewChat';
+import RootLayout from './pages/root-layout/RootLayout';
 import Chat from './pages/chat/Chat';
 
 const router = createBrowserRouter([
-    { path: '/', element: <HomePage /> },
-    { path: '/new-chat', element: <NewChat /> },
-    { path: '/chat', element: <Chat /> },
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: '/new-chat', element: <NewChat /> },
+      { path: '/chat', element: <Chat /> },
+    ],
+  },
 ]);
 
 function App() {
-    return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- front/src/pages/chat/Chat.jsx
+++ front/src/pages/chat/Chat.jsx
@@ -89,4 +89,3 @@ function Chat() {
 }
 
 export default Chat;
-
