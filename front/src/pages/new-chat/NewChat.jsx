8ece44c5d7788ab517de3c098fea2070292373bd
Nadav Elkayam <nadavelkayam12@gmail.com>
2025-04-13 22:04:29 +0300

    finished chat page

--- front/src/pages/new-chat/NewChat.jsx
+++ front/src/pages/new-chat/NewChat.jsx
@@ -1,14 +1,31 @@
 import ChatTextBox from '../../components/chat-text-box/ChatTextBox';
-import './newChat.css';
+import styles from './newChat.module.css';
+
 
 function NewChat() {
+
   return (
-    <div className="chat-container">
-      <div className="chat-content">
-        <h1 className="chat-title">Let's plan your next vacation</h1>
-        <ChatTextBox />
+    <>
+      <div className={styles['new-chat-container']}>
+      
+      <div className={styles['button-container']}>
+         <button className={styles['sidebar-btn']}>
+         <img src="src/assets/images/sidebar.png" alt="Open Sidebar" />
+         </button>
+         <button className={styles['another-btn']}>
+         <img src="src/assets/images/chat-bubble.png" alt="New chat" />
+         </button>
       </div>
-    </div>
+
+
+      <div className={styles['chat-container']}>
+        <div className={styles['chat-content']}>
+        <img src="/src/assets/images/suiteCaseLogo-removebg-preview.png" alt="Logo" className={styles['suiteCaseLogo']} /><h1 className={styles['chatTitle']}>Let's plan your next vacation</h1>
+          <ChatTextBox />
+        </div>
+      </div>
+      </div>
+    </>
   );
 }
 
