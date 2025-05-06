a46c2d73aa09ea040980d3ee246632e017c99b29
NetanelAlis <netanelalis11@gmail.com>
2025-05-06 17:58:22 +0300

    feat: add NavBar component and integrate with RootLayout; update styles and user details handling

--- front/src/pages/root-layout/RootLayout.jsx
+++ front/src/pages/root-layout/RootLayout.jsx
@@ -1,82 +1,15 @@
-import styles from './RootLayout.module.css';
-import { Link } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import NavBar from '../../components/UI/nav-bar/NavBar';
 
 export default function RootLayout() {
-    const logoutUrl = `${import.meta.env.VITE_COGNITO_LOGOUT_URL}?client_id=${
-        import.meta.env.VITE_COGNITO_CLIENT_ID
-    }&logout_uri=${import.meta.env.VITE_COGNITO_LOGOUT_REDIRECT_URI}`;
+  const rawUserDetails = localStorage.getItem('userDetails');
+  const [userDetails, setUserDetails] = useState(rawUserDetails);
 
-    const rawUserDetails = localStorage.getItem('userDetails');
-    let userDetails = null;
-
-    if (rawUserDetails) {
-        try {
-            userDetails = JSON.parse(rawUserDetails);
-        } catch (error) {
-            userDetails = null;
-        }
-    }
-
-    return (
-        <div>
-            <nav className={styles['navbar']}>
-                <Link to="/" className={styles['navbar-logo']}>
-                    <div className={styles['logo']}>
-                        <img src="/images/logo.png" alt="TripTailor Logo" />
-                        <span className={styles['navbar-website-name']}>
-                            Trip Tailor
-                        </span>
-                    </div>
-                </Link>
-
-                <ul className={styles['nav-links']}>
-                    <li>
-                        <Link to="/" className={styles['nav-link']}>
-                            Home
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/new-chat" className={styles['nav-link']}>
-                            My Trips
-                        </Link>
-                    </li>
-                    <li>
-                        <a href="#" className={styles['nav-link']}>
-                            Destinations
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className={styles['nav-link']}>
-                            Hotels
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className={styles['nav-link']}>
-                            Attractions
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className={styles['nav-link']}>
-                            Contact
-                        </a>
-                    </li>
-                    {userDetails && (
-                        <li>
-                            <a
-                                href={logoutUrl}
-                                className={styles['nav-link']}
-                                onClick={() => {
-                                    localStorage.removeItem('userDetails');
-                                }}
-                            >
-                                Log Out
-                            </a>
-                        </li>
-                    )}
-                </ul>
-            </nav>
-            <Outlet />
-        </div>
-    );
+  return (
+    <div>
+      <NavBar userDetails={userDetails} setUserDetails={setUserDetails} />
+      <Outlet context={{ handleLogIn: () => setUserDetails(true) }} />
+    </div>
+  );
 }
